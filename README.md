715ba031fad81512d8a6f89d13a6605b83053cfe
Rami Rafee <kingramy23@gmail.com>
2023-02-02 13:48:33 +0200

    feat:first-program

---
+++ README.md
@@ -0,0 +1,4 @@
+# AI-IEEE-CS23-ZSB
+AI-IEEE-CS23-ZSB TASKS
+### IEEE-AI
+![IEEE](https://ieee-zsb.org/images/Sliders/Final1300800.jpg)
---
+++ Tasks/Task 1/P1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 13:31:31 2023
+
+@author: Rami
+"""
+print("Input:")
+list1 = input()
+list1 = [int(x) for x in list1.split() ] #list1 is now a list of integers
+
+list1 = set(list1)
+list1 = list(list1)##to remove duplicates
+
+list1.sort()
+
+if(len(list1)>1):
+    print(list1[len(list1)-2])
+    print(list1[1])
+else:
+    print(list1[0])
+    print(list1[0])
+
