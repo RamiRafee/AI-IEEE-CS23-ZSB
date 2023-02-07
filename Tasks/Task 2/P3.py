0472f384cbc747bd5445ac4d111157e31b84935a
Rami Rafee <kingramy23@gmail.com>
2023-02-07 11:17:51 +0200

    fix : task2 P3

--- Tasks/Task 2/P3.py
+++ Tasks/Task 2/P3.py
@@ -33,9 +33,10 @@ if __name__ == "__main__":
             maxSub.append(array[i])
             
         ## to handle the [Len(sublist) > 1]
-        elif(array[i] + maxSum > maxSum and array[i] == maxSub[0] and len(maxSub)==1):
-            maxSum = array[i - 1] + maxSum
-            maxSub.append(array[i-1])
+        # elif(array[i] + maxSum > maxSum and array[i] == maxSub[0] and len(maxSub)==1):
+        #     maxSum = array[i - 1] + maxSum
+        #     maxSub.append(array[i-1])
+            
         ##
         
         elif(array[i] + minSum< minSum and array[i] != minSub[0]):
@@ -43,11 +44,18 @@ if __name__ == "__main__":
             minSub.append(array[i])
             
         ## to handle the [Len(sublist) > 1]
-        elif(array[i] + minSum< minSum and array[i] == minSub[0] and len(minSub)==1):
-            minSum = array[i+1] + minSum
-            minSub.append(array[i+1])
-        ##
-            
+        # elif(array[i] + minSum< minSum and array[i] == minSub[0] and len(minSub)==1):
+        #     minSum = array[i+1] + minSum
+        #     minSub.append(array[i+1])
+        
+        ##to handle the [Len(sublist) > 1]
+    if(len(maxSub) ==1 ):
+        maxSub.append(array[len(array)-2])
+        maxSum = array[len(array)-2] + maxSum
+    if(len(minSub) ==1 ):
+        minSub.append(array[1])
+        minSum = array[1] + minSum
+    
     print("OUTPUT:")
     print(maxSub," ({})".format(maxSum))
     print(minSub," ({})".format(minSum))
